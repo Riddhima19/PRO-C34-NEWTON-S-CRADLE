bdb55259e7490a3d7dc96ceaf0311b1f00d7fa5d
Riddhima19 <74543205+Riddhima19@users.noreply.github.com>
2021-01-06 20:02:13 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,31 @@
+class Rope{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:1.2,
+            angularStiffness : 1,    
+            length:220
+        }
+        this.pointB = pointB;
+        this.rope = Constraint.create(options);
+        World.add(world,this.rope);
+    }
+    attach(body){
+        this.rope.bodyA = body;
+    }
+
+    fly(){
+        this.rope.bodyA = null;
+    }
+    display(){
+        if(this.rope.bodyA){
+            var pointA = this.rope.bodyA.position;
+            var pointB= this.pointB;
+            push(); 
+            fill("black")
+            line(pointB.x,pointB.y,pointA.x,pointA.y);
+            pop();
+        }
+    }
+}
